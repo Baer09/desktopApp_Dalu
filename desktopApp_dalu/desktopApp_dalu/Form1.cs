47a87c50ee9833eb8b788daf2a1d9ccd7a3d5dd8
Baer <brayan.escobar@rocketmail.com>
2026-09-03 22:14:38 -0600

    test

--- desktopApp_dalu/desktopApp_dalu/Form1.cs
+++ desktopApp_dalu/desktopApp_dalu/Form1.cs
@@ -16,5 +16,10 @@ namespace desktopApp_dalu
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // call to inventory form
+        }
     }
 }
